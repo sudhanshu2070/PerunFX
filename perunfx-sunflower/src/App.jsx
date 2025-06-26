436eb83166b7a9dd6e90d71327fbac1d36d8b4ac
sudhanshu2070 <sssallen2016@gmail.com>
2025-06-26 23:37:06 +0530

    lifecycle comp 0.2

--- perunfx-sunflower/src/App.jsx
+++ perunfx-sunflower/src/App.jsx
@@ -1,9 +1,7 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import PropComp from './PropComp'
-import LifeCycleComp from './LifeCycleComp'
+import PropComp from './Components/PropComp'
+import LifeCycleComp from './Components/LifeCycleComp'
 
 function App() {
   const [count, setCount] = useState(0)
