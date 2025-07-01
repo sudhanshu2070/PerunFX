545a6c2d33442013786a9a61086951075cdbb834
sudhanshu2070 <sssallen2016@gmail.com>
2025-07-01 23:25:59 +0530

    Use context 0.2

--- perunfx-sunflower/src/App.jsx
+++ perunfx-sunflower/src/App.jsx
@@ -4,11 +4,14 @@ import PropComp from './Components/PropComp';
 import LifeCycleComp from './Components/LifeCycleComp';
 import FileUpload from './Components/FileUpload';
 import GridGame from './Components/GridGame';
+import ThemeContext from './utils/ThemeContext';
 
 function App() {
   const [count, setCount] = useState(0)
+  const [theme, setTheme] = useState('light');
 
   return (
+    <ThemeContext.Provider value={{ theme, setTheme }}>
       <div>
         {/* <PropComp prop={101} propFunc={()=> alert('hey there')} /> */}
         <PropComp prop={1001} propFunc={() => console.log('testing prop function')} />
@@ -16,6 +19,7 @@ function App() {
         {/* <FileUpload /> */}
         <GridGame />
       </div>
+      </ThemeContext.Provider>
   )
 }
 
